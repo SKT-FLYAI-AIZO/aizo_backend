0d6de19aa9e546fbffb060f3ba5c4bf76b5e7efd
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2022-08-26 10:29:51 +0900

    Modify storage api

--- storage/views.py
+++ storage/views.py
@@ -18,36 +18,35 @@ class VideoUploaderView(View):
         if video_file is None:
             return JsonResponse({"message": "There is no video file..."}, status=400)
 
-        loc_file = request.FILES.get('loc_file')
-        if loc_file is None:
-            return JsonResponse({"message": "There is no loc file..."}, status=400)
+        loc = request.POST.get('loc')
+        if loc is None:
+            return JsonResponse({"message": "There is no loc..."}, status=400)
 
         email = request.POST.get('email')
-        location = request.POST.get('location')
+        if email is None:
+            return JsonResponse({"message": "There is no email..."}, status=400)
+
         date = request.POST.get('date')
+        if date is None:
+            return JsonResponse({"message": "There is no date..."}, status=400)
 
-        if email is None or location is None or date is None:
-            return JsonResponse({"message": "Check info. Fill all data."}, status=400)
-
-        video_filename = str(email) + "_" + str(location) + "_" + str(date) + ".mp4"
-        loc_filename = str(email) + "_" + str(location) + "_" + str(date) + ".txt"
+        video_filename = str(email) + "_" + date + ".mp4"
 
         try:
             blob_service_client = BlobServiceClient.from_connection_string(STORAGE_CONNECTION_STRING)
             blob_video_client = blob_service_client.get_blob_client(container=my_storage.azure_container, blob=video_filename)
-            blob_loc_client = blob_service_client.get_blob_client(container=my_storage.azure_container, blob=loc_filename)
-
             blob_video_client.upload_blob(video_file, overwrite=True)
-            blob_loc_client.upload_blob(loc_file, overwrite=True)
         except Exception:
             return JsonResponse({"message": "Upload failed"}, status=400)
 
         query = Account.objects.filter(email=email).only("id")
+        if query.__len__() == 0:
+            return JsonResponse({"message": "There is no such email."}, status=204)
         account_id = int(query.get().id)
 
         Video.objects.create(
             date=datetime.strptime(' '.join(date.split('-')), '%Y %m %d'),
-            location=location,
+            location="test location",
             account_id_id=account_id,
             path=MEDIA_URL + video_filename
         ).save()
